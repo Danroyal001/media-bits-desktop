d335b9ca1d21b6477fa0ccb66655f1312e02e575
Daniel Effiong <danroyal001@pop-os.localdomain>
2020-11-03 14:10:21 +0100

    Nov 3 2020 - 2:10pm

--- src/mainProcess/index.js
+++ src/mainProcess/index.js
@@ -1,7 +1,9 @@
-const { app, BrowserWindow, ipcMain } = require('electron');
+const { app, BrowserWindow, ipcMain, Tray, Menu, desktopCapturer } = require('electron');
 const path = require('path');
 const minWidth = 1000;
 const minHeight = 600;
+let appTrayIcon = null;
+const iconPath = path.join(__dirname, '..', 'rendererProcess', 'globalAssets', 'logo.png');
 // const httpServer = require('./http-server');
 
 // httpServer('../rendererProcess/windows/main', 8085);
@@ -11,12 +13,22 @@ if (require('electron-squirrel-startup')) { // eslint-disable-line global-requir
   app.quit();
 }
 
+// handle minimize and maximize
+ipcMain.on('minimize', e => BrowserWindow.fromId(e.sender.id).minimize())
+ipcMain.on('maximize', e => {
+  const win = BrowserWindow.fromId(e.sender.id)
+  if(win.isMaximized()){
+    win.unmaximize()
+    win.setSize(minWidth, minHeight, true)
+  } else win.maximize()
+});
+
 const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: minWidth,
     height: minHeight,
-    icon: path.join(__dirname, '..', 'rendererProcess', 'globalAssets', 'logo.png'),
+    icon: iconPath,
     frame: false,
     webPreferences: {
       scrollBounce: true,
@@ -28,36 +40,53 @@ const createWindow = () => {
 
   // mainWindow.loadFile(path.join(__dirname, 'index.html'));
   mainWindow.loadURL("http://localhost:8080/#/editor");
+  // set id to random float
+  const id = Math.random();
+  mainWindow.id = id;
+  mainWindow.webContents.id = id;
   mainWindow.menuBarVisible = false;
   mainWindow.setBackgroundColor('#009688');
   mainWindow.setMinimumSize(minWidth, minHeight)
   mainWindow.maximize();
   // Open the DevTools.
-  //mainWindow.webContents.openDevTools();
-  // set id to random float
-  mainWindow.id = Math.random()
-  // handle minimize and maximize
-  ipcMain.on('minimize', e => BrowserWindow.fromId(e.sender.id).minimize())
-  ipcMain.on('maximize', e => {
-    const win = BrowserWindow.fromId(e.sender.id)
-    if(win.isMaximized()){
-      win.unmaximize()
-      win.setSize(minWidth, minHeight, true)
-    } else win.maximize()
-  })
+  mainWindow.webContents.openDevTools();
   // end open window function
 };
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.on('ready', () => {
+  createWindow()
+  // add to app tray
+  appTrayIcon = new Tray(iconPath);
+  const contextMenu = Menu.buildFromTemplate([
+    {
+    label: 'New Window',
+    click: () => {
+      createWindow();
+      }
+    },
+    {
+      label: 'Quit/Close',
+      click: () => {
+        app.quit();
+        }
+      }
+  ])
+  appTrayIcon.setToolTip('Electron Demo in the tray.')
+  appTrayIcon.setContextMenu(contextMenu)
+  });
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
+    if (appTrayIcon) {
+      appTrayIcon.destroy()
+      appTrayIcon = null;
+    }
     app.quit();
   }
 });
